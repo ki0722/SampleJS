b6a114322cdd76eb04c38bcd4e9c01e6af5ce17b
ki0722 <64395183+ki0722@users.noreply.github.com>
2020-06-19 13:50:13 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -50,3 +50,22 @@ function MyFunction032(){
           obj[ i ].style.backgroundColor = "blue";
       }
     }
+
+    // 引数を用いた背景色の変更
+function MyFunction05( col ){
+  var obj = document.body;
+  obj.style.backgroundColor = col;
+}
+
+
+//ランダムな16進6桁のカラーコードを生成（汎用性のある関数）
+function RandomColor(){
+    var col = Math.floor( Math.random() * 0xFFFFFF ).toString(16);
+    for( var i = col.length; i<6; i++ ) { col = '0' + col; }
+    return '#' + col;
+}
+// 背景色の変更
+function MyFunction06( ){
+  var obj = document.body;
+  obj.style.backgroundColor = RandomColor();
+}
